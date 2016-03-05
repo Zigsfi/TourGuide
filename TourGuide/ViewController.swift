69ec27a6a5219f3188f5c6345b47502c42d8584e
aberman <arthurberman94@gmail.com>
2016-03-05 12:04:49 -0500

    Initial Commit

---
+++ TourGuide/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TourGuide
+//
+//  Created by Arthur Berman on 3/5/16.
+//  Copyright © 2016 Arthur Berman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
